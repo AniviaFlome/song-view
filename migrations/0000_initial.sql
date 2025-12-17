58854ade9309bf1ae5ca7390879d1e4a41e2eaa2
AniviaFlome <aniviaflome@gmail.com>
2025-12-17 17:50:34 +0300

    Add D1 database binding for share feature

--- migrations/0000_initial.sql
+++ migrations/0000_initial.sql
@@ -1,4 +1,4 @@
--- Migration number: 0000 	 2024-01-01T00:00:00.000Z
+-- Migration number: 0000 	 2025-12-17T17:38:24.000Z
 CREATE TABLE IF NOT EXISTS shares (
     id TEXT PRIMARY KEY,
     data TEXT NOT NULL,
